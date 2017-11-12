0788a50a830dd6d88a9b315fd6efbad2ebf3ef18
Ryan Coughlin <ryan@ryancoughlin.com>
2017-11-12 12:57:46 -0500

    Add in SASS and base styling

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,4 @@
 import React, { Component } from "react"
-import PropTypes from "prop-types"
-import PostIcons from "../components/PostIcons"
-
-import { rhythm } from "../utils/typography"
-
 import Helmet from "react-helmet"
 
 class PageTemplate extends Component {
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,28 +1,18 @@
 import React, { Component } from "react"
-import PropTypes from "prop-types"
-import PostIcons from "../components/PostIcons"
 import Img from "gatsby-image"
-
-import { rhythm } from "../utils/typography"
 
 class PostTemplate extends Component {
   render() {
     const post = this.props.data.wordpressPost
-    console.log('hey', post)
 
     return (
       <div>
         <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />       
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        })}
       </div>
     )
   }
-}
-//<img src={post.image.sizes.thumbnail} />
-
-PostTemplate.propTypes = {
-  data: PropTypes.object.isRequired,
-  edges: PropTypes.array,
 }
 
 export default PostTemplate
@@ -32,6 +22,9 @@ export const pageQuery = graphql`
     wordpressPost(id: { eq: $id }) {
       title
       content
+      acf {
+        flies
+      }
     }
     site {
       siteMetadata {
