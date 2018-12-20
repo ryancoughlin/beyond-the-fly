7bf10d5ceaf4c30430ac8f03119e3e54cf77ebe6
Ryan Coughlin <ryan@ryancoughlin.com>
2018-12-19 22:25:34 -0500

    Fix path

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,11 @@ import React from 'react'
 import './normalize.css'
 import './layout.css'
 
+import logo from '../images/logo.svg'
+
 const Layout = ({ children }) => (
   <div class="container">
-    <img src="images/logo.svg" alt="Beyond the Fly" width={300} />
+    <img src={logo} alt="Beyond the Fly" width={300} />
   </div>
 )
 
