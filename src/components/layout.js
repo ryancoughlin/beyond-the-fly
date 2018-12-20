e2832fbacfd33d6abdace665307789d244e3a99a
Ryan Coughlin <ryan@ryancoughlin.com>
2018-12-19 21:56:14 -0500

    Add logo, remove old code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import './normalize.css'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+  <div class="container">
+    <img src="images/logo.svg" alt="Beyond the Fly" width={300} />
+  </div>
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
